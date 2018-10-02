9d08f711868188d5c9eaa76fea6ee55e7b7db15e
Quincy Le <lexuqing8@gmail.com>
2018-10-02 13:36:16 -0400

    update ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -18,61 +18,13 @@ module.exports = {
       }
     },
     {
-      name: 'ember-lts-2.12',
+      name: 'ember-212',
       bower: {
         dependencies: {
           'ember': 'components/ember#lts-2-12'
         },
         resolutions: {
           'ember': 'lts-2-12'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
         }
       },
       npm: {
