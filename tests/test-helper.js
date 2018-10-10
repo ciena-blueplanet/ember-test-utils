0d061bf39a521fb2ea5fa33f885e0d2b49a5d3d3
Quincy Le <lexuqing8@gmail.com>
2018-10-10 10:51:29 -0400

    remove comments

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,3 @@
-// import resolver from './helpers/resolver'
-// import {setResolver} from 'ember-mocha'
-
-// setResolver(resolver)
-
 import Application from '../app'
 import config from '../config/environment'
 import {setApplication} from '@ember/test-helpers'
